4c854dfe89583b95881d3c809d2f85ead2268a1f
frankrodelli <frankrodelli93@gmail.com>
2018-10-01 22:58:03 -0400

    Final Commit. Partial Picture functionality

--- app/src/main/java/com/bignerdranch/android/module2/CreateCustomer.java
+++ app/src/main/java/com/bignerdranch/android/module2/CreateCustomer.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +17,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CreateCustomer extends AppCompatActivity {
 
@@ -22,6 +31,8 @@ public class CreateCustomer extends AppCompatActivity {
     private ModuleFiveDatabase mfd;
     private String mFirstName;
     private String mLastName;
+
+    private File customerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +45,7 @@ public class CreateCustomer extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: Implement camera functionality
-                Toast.makeText(CreateCustomer.this, "This will allow you to take" +
-                                " customer picture with fragment overlay",
-                        Toast.LENGTH_SHORT).show();
+                dispatchTakePictureIntent();
             }
         });
 
@@ -60,10 +68,48 @@ public class CreateCustomer extends AppCompatActivity {
                 Log.e("Customer added", mFirstName+ " " + mLastName);
                 String newList = DatabaseUtils.dumpCursorToString(mfd.getCustotmers());
                 Log.e("This is the list", newList);
-
                 //Back to customer page
                 finish();
             }
         });
     }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        String imageName = mFirstNameEditText.getText().toString();
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = new File(storageDir, imageName + ".jpg");
+        image.createNewFile();
+
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
--- app/src/main/java/com/bignerdranch/android/module2/Customers.java
+++ app/src/main/java/com/bignerdranch/android/module2/Customers.java
@@ -2,6 +2,9 @@ package com.bignerdranch.android.module2;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,14 +14,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Customers extends AppCompatActivity {
 
     private ModuleFiveDatabase mfd;
     private ListView myListView;
+
+    private ImageView mCustomerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +62,16 @@ public class Customers extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mCustomerImage = findViewById(R.id.customerImage);
+
+        /*try {
+            FileInputStream cusim = openFileInput("Frank.jpeg");
+        } catch (FileNotFoundException e) {
+            e.printar;
+        }*/
+
+        Log.e("Here", Arrays.toString(fileList()));
     }
 
     private void populateListView() {
@@ -57,6 +82,7 @@ public class Customers extends AppCompatActivity {
         //Populate list with sessions
         myListView = findViewById(R.id.session_list);
         final ArrayList<String> sessionItems = new ArrayList<String>();
+        final ArrayList<String> sessionIcons = new ArrayList<>();
 
 
         for(cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()){
@@ -64,18 +90,19 @@ public class Customers extends AppCompatActivity {
                     cursor.getString(2);
             Log.d("this is the customer", customer);
             sessionItems.add(customer);
+
+            //add items to customer picture arraylist
+            String icon = cursor.getString(1) + ".jpg";
+            sessionIcons.add(icon);
+
         }
 
-        final ArrayAdapter<String > aa;
-        aa = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,sessionItems);
-
-        myListView.setAdapter(aa);
+        myListView.setAdapter(new ArrayAdapter<String>(
+                this, R.layout.customer_list, R.id.Itemname, sessionItems));
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         populateListView();
         myListView.invalidate();
     }
-
 }
